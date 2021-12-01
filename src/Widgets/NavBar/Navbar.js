deacee3420593504c64eca9c2c117b9535ca38db
Sofiree <muosophia0@gmail.com>
2021-12-01 23:43:41 +0100

    Bootstrapped Navbar, created buttons component

--- src/Widgets/NavBar/Navbar.js
+++ src/Widgets/NavBar/Navbar.js
@@ -2,82 +2,98 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <React.Fragment>
-      <nav>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <ul>
-            <li>
-              <NavLink to="/" className="nav-link">
-                MUSIC
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/" className="nav-link">
-                ARTIST
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/" className="nav-link">
-                EVENTS
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/" className="nav-link">
-                COMMERCIAL
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/" className="nav-link">
-                RADIO
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/" className="nav-link">
-                GOLD
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/" className="nav-link">
-                SHOP
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/" className="nav-link">
-                BLOG
-              </NavLink>
-            </li>
-            <li>tiny logo</li>
-          </ul>
-        </div>
-      </nav>
-      <nav>
-        <div>
-          <ul>
-            <li>
-              <NavLink to="/" className="nav-link">
-                PLAYER
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/" className="nav-link">
-                SIGN IN
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/" className="nav-link">
-                SEARCH
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-        <div>music player widget goes here</div>
-      </nav>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <NavLink to="/"><img src="" alt="" className="navbar-brand" /></NavLink>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link">
+                                    MUSIC
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link">
+                                    ARTIST
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link">
+                                    EVENTS
+                                </NavLink>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <NavLink to="/" className="dropdown-toggle nav-link" role="button" id="dropdownMenuLink1" data-bs-toggle="dropdown" aria-expanded="false" >
+                                    COMMERCIAL
+                                </NavLink>
+                                <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink1">
+                                    <li><NavLink className="dropdown-item" to="/">Action</NavLink></li>
+                                </ul>
+                            </li>
+
+                            <li className="nav-item dropdown">
+                                <NavLink to="/" className="dropdown-toggle nav-link" role="button" id="dropdownMenuLink2" data-bs-toggle="dropdown" aria-expanded="false" >
+                                    RADIO
+                                </NavLink>
+                                <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink2">
+                                    <li><NavLink className="dropdown-item" to="/">Action</NavLink></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link">
+                                    GOLD
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link">
+                                    SHOP
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link">
+                                    BLOG
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">tiny logo</li>
+                        </ul>
+                    </div>
+                    {/* Second nav bar */}
+                    <nav className="navbar navbar-expand-lg">
+                        <div className="container-fluid">
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <NavLink to="/" className="nav-link">
+                                        PLAYER
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink to="/" className="nav-link">
+                                        SIGN IN
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink to="/" className="nav-link">
+                                        SEARCH
+                                    </NavLink>
+                                </li>
+                            </ul>
+                            </div>
+                        </div>
+                        <div>music player widget goes here</div>
+                    </nav>
+                </div>
+            </nav>
+
+
+        </React.Fragment>
+    );
 };
 
 export { Navbar };
