55827bba820218e26a99dc339127bc6de0427758
Sofiree <muosophia0@gmail.com>
2021-12-01 21:46:41 +0100

    created the Nav bar component

---
+++ src/Widgets/NavBar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar=()=>{
+    return(
+        <React.Fragment>
+            <nav>
+                whewhiwebfwioue
+            </nav>
+        </React.Fragment>
+    )
+}
+
+export {Navbar};
